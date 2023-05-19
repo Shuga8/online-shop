38e9cd835129648fea9ace30059683668ddbc863
Shuga8 <lotocharles8@gmail.com>
2023-05-19 20:12:26 +0100

    new product validation
    completed

--- app/controllers/Admins.php
+++ app/controllers/Admins.php
@@ -74,7 +74,77 @@ class Admins extends Controller
 
             //Image validation
 
-            if (isset($_FILE['product_img'])) {
+            if (isset($_FILES['product_img'])) {
+
+                //Get all file parameters/values
+                $imgName = $_FILES['product_img']['name'];
+                $imgSize = $_FILES['product_img']['size'];
+                $imgTmpName = $_FILES['product_img']['tmp_name'];
+                $imgErr =
+                    $_FILES['product_img']['error'];
+
+                // echo $imgName . "<br>" . $imgSize . "<br>" . $imgTmpName . "<br>" . $imgErr;
+                $fileTypes = ['jpg', 'png', 'jpeg'];
+                $imgExtension = explode(".", $imgName);
+                $imgExtension = strtolower($imgExtension[1]);
+
+                //Check if image has an error while been uploaded
+                if ($imgErr == true) {
+                    $_SESSION['error'] = "<span style='color: red;'>Error: Something is wrong with this file!</span>";
+                    header("Location: " . SITE_URL . "/Admins/new");
+                    exit(0);
+                } else {
+
+                    //Check if image's file extension is supported
+                    if (!in_array($imgExtension, $fileTypes)) {
+                        $_SESSION['error'] = "<span style='color: red;'>Error: Supported image types are only 'jpg', 'jpeg', and  'png'</span>";
+                        header("Location: " . SITE_URL . "/Admins/new");
+                        exit(0);
+                    } else {
+
+                        //check if file exceeds maximum file size limit
+                        if ($imgSize > 3000000) {  //if it exceeds 3MB
+                            $_SESSION['error'] = "<span style='color: red;'>Error: Images should not exceed 3MB!!</span>";
+                            header("Location: " . SITE_URL . "/Admins/new");
+                            exit(0);
+                        } else {
+
+                            //Rename File
+                            $newImgName = time() . $imgName;
+
+                            //upload file to Extras folder
+                            $upload = move_uploaded_file($imgTmpName, "../public/extras/" . $newImgName);
+
+                            //Check if upload is successfull
+                            if ($upload == true) {
+
+                                $data['imageName'] = $newImgName;
+                                $data['p_id'] = uniqid() . $data['p_name'];
+
+                                //Call Model upload Method
+                                $uploader = $this->adminModel->store_new_product($data);
+
+                                if ($uploader == true) {
+                                    $_SESSION['error'] = "<span style='color: green;'>Product added successfully &check;</span>";
+                                    header("Location: " . SITE_URL . "/Admins/new");
+                                    exit(0);
+                                } else {
+                                    $_SESSION['error'] = "<span style='color: red;'>Error: could not perform action</span>";
+                                    header("Location: " . SITE_URL . "/Admins/new");
+                                    exit(0);
+                                }
+                            } else {
+                                $_SESSION['error'] = "<span style='color: red;'>Sorry an error occured,  please try again!!</span>";
+                                header("Location: " . SITE_URL . "/Admins/new");
+                                exit(0);
+                            }
+                        }
+                    }
+                }
+            } else {
+                $_SESSION['error'] = "<span style='color: red;'>Error: Please select an image!</span>";
+                header("Location: " . SITE_URL . "/Admins/new");
+                exit(0);
             }
         }
     }
--- app/models/Admin.php
+++ app/models/Admin.php
@@ -8,4 +8,28 @@ class Admin
     {
         $this->db = new Database();
     }
+
+    // A public function to store newly added products
+    public function store_new_product($data)
+    {
+        // Query to insert new product to the database
+        $this->db->query("INSERT INTO products(product_id, product_name, product_caption, product_image, product_price, product_category, product_size, product_quantity) VALUES(:id, :name, :cap, :img, :price, :cat, :size, :quantity)");
+
+        //Bind prepared valued to params
+        $this->db->bind(':id', $data['p_id']);
+        $this->db->bind(':name', $data['p_name']);
+        $this->db->bind(':cap', $data['p_cap']);
+        $this->db->bind(':img', $data['imageName']);
+        $this->db->bind(':price', $data['p_price']);
+        $this->db->bind(':cat', $data['p_category']);
+        $this->db->bind(':size', $data['p_size']);
+        $this->db->bind(':quantity', $data['p_quantity']);
+
+        //Execute query
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
