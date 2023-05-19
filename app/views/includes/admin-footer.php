fd32becef04365dee02b33d41a84bb65697e4216
Shuga8 <lotocharles8@gmail.com>
2023-05-19 14:17:30 +0100

    admin layout styling completed

--- app/views/includes/admin-footer.php
+++ app/views/includes/admin-footer.php
@@ -1,3 +1,4 @@
+</main>
 <script src="<?php echo SITE_URL; ?>/public/js/header.js"></script>
 </body>
 
--- app/views/includes/admin-header.php
+++ app/views/includes/admin-header.php
@@ -10,3 +10,43 @@
     <link rel="stylesheet" href="<?php echo SITE_URL; ?>/public/css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
+<body>
+
+
+    <aside class="aside" id="aside">
+
+        <div class="logo">
+            <i class="fa-solid fa-cart-plus"></i>
+        </div>
+
+        <div class="links">
+            <a href=""><i class="fa-solid fa-gauge"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Dashboard</span></a>
+            <a href=""><i class="fa-solid fa-boxes-stacked"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Manage products</span></a>
+            <a href=""><i class="fa-solid fa-circle-plus"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Product</span></a>
+            <a href=""><i class="fa-solid fa-user-plus"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Add Admin</span></a>
+            <a href=""><i class="fa-solid fa-power-off"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Sign out</span></a>
+        </div>
+
+    </aside>
+
+    <main id="main">
+        <header id="header" class="header">
+            <nav id="navbar">
+                <a href="javascript:void(0)" id="toggle-icon">&#9776;</a>
+            </nav>
+
+            <div class="other-nav">
+
+                <div class="country-flag">
+                    <img src="<?php echo SITE_URL; ?>/public/images/nigeria-flag.png" alt="User icon /">
+                </div>
+
+                <div class="user-icon">
+                    <img src="<?php echo SITE_URL; ?>/public/images/user_icon.png" alt="User icon /">
+                </div>
+
+                <a href=""><i class="fas fa-gear"></i> Settings</a>
+
+            </div>
+        </header>
