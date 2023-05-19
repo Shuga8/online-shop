d51723cd57b31697c3455d6c943d16d3d1a0b0d7
Shuga8 <lotocharles8@gmail.com>
2023-05-19 14:12:05 +0100

    css progressing

--- app/views/Admin/index.php
+++ app/views/Admin/index.php
@@ -13,7 +13,15 @@ include_once APPROOT . "/views/includes/admin-header.php";
     <aside class="aside" id="aside">
 
         <div class="logo">
-            <i class="fas fa-smile-wink    "></i>
+            <i class="fa-solid fa-cart-plus"></i>
+        </div>
+
+        <div class="links">
+            <a href=""><i class="fa-solid fa-gauge"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Dashboard</span></a>
+            <a href=""><i class="fa-solid fa-boxes-stacked"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Manage products</span></a>
+            <a href=""><i class="fa-solid fa-circle-plus"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Product</span></a>
+            <a href=""><i class="fa-solid fa-user-plus"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Add Admin</span></a>
+            <a href=""><i class="fa-solid fa-power-off"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Sign out</span></a>
         </div>
 
     </aside>
@@ -23,6 +31,20 @@ include_once APPROOT . "/views/includes/admin-header.php";
             <nav id="navbar">
                 <a href="javascript:void(0)" id="toggle-icon">&#9776;</a>
             </nav>
+
+            <div class="other-nav">
+
+                <div class="country-flag">
+                    <img src="<?php echo SITE_URL; ?>/public/images/nigeria-flag.png" alt="User icon /">
+                </div>
+
+                <div class="user-icon">
+                    <img src="<?php echo SITE_URL; ?>/public/images/user_icon.png" alt="User icon /">
+                </div>
+
+                <a href=""><i class="fas fa-gear"></i> Settings</a>
+
+            </div>
         </header>
     </main>
 
