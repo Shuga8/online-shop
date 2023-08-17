6c3d45f1c5c112e5af35f900ccefd3a4a8f3170d
Shuga8 <lotocharles8@gmail.com>
2023-08-17 18:57:10 +0100

    pushing

--- app/views/Pages/shop.php
+++ app/views/Pages/shop.php
@@ -321,39 +321,43 @@ include_once APPROOT . "/views/includes/header.php";
               <h2>Categories</h2>
             </div>
           </div>
-          <div class="row">
-            <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
-              <a class="block-2-item" href="#">
-                <figure class="image">
-                  <img src="<?php echo SITE_URL;  ?>/public/images/women.jpg" alt="" class="img-fluid">
-                </figure>
-                <div class="text">
-                  <span class="text-uppercase">Collections</span>
-                  <h3>Women</h3>
+          <div class="site-section site-blocks-2">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
+                  <a class="block-2-item" href="<?php echo SITE_URL; ?>/pages/shop/category/women">
+                    <figure class="image">
+                      <img src="<?php echo SITE_URL; ?>/public/images/women.jpg" alt="" class="img-fluid">
+                    </figure>
+                    <div class="text">
+                      <span class="text-uppercase">Collections</span>
+                      <h3>Women</h3>
+                    </div>
+                  </a>
                 </div>
-              </a>
-            </div>
-            <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
-              <a class="block-2-item" href="#">
-                <figure class="image">
-                  <img src="<?php echo SITE_URL;  ?>/public/images/children.jpg" alt="" class="img-fluid">
-                </figure>
-                <div class="text">
-                  <span class="text-uppercase">Collections</span>
-                  <h3>Children</h3>
+                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+                  <a class="block-2-item" href="<?php echo SITE_URL; ?>/pages/shop/category/children">
+                    <figure class="image">
+                      <img src="<?php echo SITE_URL; ?>/public/images/children.jpg" alt="" class="img-fluid">
+                    </figure>
+                    <div class="text">
+                      <span class="text-uppercase">Collections</span>
+                      <h3>Children</h3>
+                    </div>
+                  </a>
                 </div>
-              </a>
-            </div>
-            <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
-              <a class="block-2-item" href="#">
-                <figure class="image">
-                  <img src="<?php echo SITE_URL;  ?>/public/images/men.jpg" alt="" class="img-fluid">
-                </figure>
-                <div class="text">
-                  <span class="text-uppercase">Collections</span>
-                  <h3>Men</h3>
+                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
+                  <a class="block-2-item" href="<?php echo SITE_URL; ?>/pages/shop/category/men">
+                    <figure class="image">
+                      <img src="<?php echo SITE_URL; ?>/public/images/men.jpg" alt="" class="img-fluid">
+                    </figure>
+                    <div class="text">
+                      <span class="text-uppercase">Collections</span>
+                      <h3>Men</h3>
+                    </div>
+                  </a>
                 </div>
-              </a>
+              </div>
             </div>
           </div>
 
